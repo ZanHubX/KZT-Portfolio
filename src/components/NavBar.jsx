fdd32941b3f28584db31dd9fbb56a438c2697d79
hosh1dEcho <kaungzanthaw27904@gmail.com>
2024-10-02 16:36:47 +0630

    last commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,6 @@ import {
   Navbar,
   MobileNav,
   Typography,
-
   IconButton,
 } from "@material-tailwind/react";
 
@@ -18,7 +17,7 @@ export function NavBar({ isDarkMode }) {
   }, []);
 
   const navList = (
-    <ul className="mt-2 mb-3 flex flex-col gap-3 lg:mb-0 lg:mt-0 lg:flex-row lg:gap-6 sticky top-0 bg-white z-10">
+    <ul className="mt-2 mb-3 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:gap-6 sticky top-0 bg-white z-10">
       <Typography
         as="li"
         variant="small"
